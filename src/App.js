6a5feaf831f760503155f7494013c92abe5382e8
Romane G <romane71193@gmail.com>
2019-06-01 18:27:57 -0400

    clicks alternate between x's and o's

--- src/App.js
+++ src/App.js
@@ -7,23 +7,33 @@ class App extends Component {
     super(props)
 
     this.state = {
-        gameBoard: Array(9).fill("X"),
+        gameBoard: Array(9).fill("tic"),
         playerTurn: 'O',
         winner: null,
     }
 
    
   }
-  markBoard = () => {
-     alert("you clicked me") 
+  markBoard = (i) => {
+   
+
+    console.log(i)
+    console.log(this.state.gameBoard[i])
+   const changeValue = this.state.gameBoard.slice(0,this.state.gameBoard.length) 
+   console.log(changeValue)
+   changeValue[i] = this.state.playerTurn
+   console.log(changeValue)
+    
   const whoTurn = this.state.playerTurn === 'O' ? "X" : "O"
       this.setState({
-        playerTurn: whoTurn
+        gameBoard: changeValue,
+        playerTurn: whoTurn,
+        winner: null
       })
   }
 
   render() {
-    let boxes = this.state.gameBoard.map((x,i) => <div onClick={this.markBoard} key={i} className="box">{this.state.playerTurn}</div>)
+    let boxes = this.state.gameBoard.map((z,i) => <div onClick={ () => this.markBoard(i)} key={i} className="box">{this.state.gameBoard[i]}</div>)
   return (
         <div>
     <div style={{ }}className="App boxcontainer">
