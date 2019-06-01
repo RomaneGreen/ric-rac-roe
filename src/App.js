7cae5c88dfe6d3c13f4e13ca6b42c38dfac5de1a
Romane G <romane71193@gmail.com>
2019-06-01 15:04:52 -0400

    squares can register clicks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React,{ Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state = {
-        gameBoard: [],
+        gameBoard: Array(9).fill("Y"),
         playerTurn: 'X',
         winner: null,
     }
 
+   
   }
+  markBoard = () => {
+     alert("you clicked me") 
+  }
+
   render() {
+    let boxes = this.state.gameBoard.map((x,i) => <div onClick={this.markBoard} key={i} className="box">{i}</div>)
   return (
         <div>
-        
     <div style={{ }}className="App boxcontainer">
-    
+          {boxes}
+
+
+
+
+      {/* <div className="box"></div>
       <div className="box"></div>
       <div className="box"></div>
       <div className="box"></div>
@@ -25,8 +36,7 @@ class App extends Component {
       <div className="box"></div>
       <div className="box"></div>
       <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
+      <div className="box"></div> */}
     </div></div>
   );
 }
